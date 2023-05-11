10dd79f8f3db92366b7229b35abbee0d6d6bf5ae
Leila Zrim <leilazrim@gmail.com>
2023-05-11 11:05:08 +0200

    added styles and content for home page

--- 01 - Introduction to React Router/09 - VanLife project bootstrapping/Home.jsx
+++ 01 - Introduction to React Router/09 - VanLife project bootstrapping/Home.jsx
@@ -1,4 +1,13 @@
+import { Link } from 'react-router-dom';
+
 const Home = () => {
-  return <div id="intro-page">Home</div>;
+  return (
+    <div id="intro-page">
+      <div id="intro-content">
+        <h2>You got the travel plans, we got the travel vans.</h2>
+        <Link to="/vans">Find your van</Link>
+      </div>
+    </div>
+  );
 };
 export default Home;
